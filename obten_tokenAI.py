17b2dd7fd03b6ea8626fbb0d36775c66b9f14325
MarceloM123 <58484864+MarceloM123@users.noreply.github.com>
2022-01-11 12:38:40 -0600

    Add files via upload

--- obten_tokenAI.py
+++ obten_tokenAI.py
@@ -1,6 +1,7 @@
 # Marcelo Márquez Murillo - A01720588
 
 import sys
+import parserAI as parser # Importamos el codigo de parser para enviarle los espacios
 
 # Tokens
 INT = 100 # Número entero
@@ -13,15 +14,18 @@ BOL = 106 # Booleanos
 END = 107 # Fin de la entrada
 ERR = 200 # Error léxico: palabra desconocida
 
+# Token especial para Espacios
+ESP = 108
+
 #    num    (    )  esp    .    #    tf  ch    "  raro   $
-MT = [[1, IZQ, DER,   0,   7,   6,   4,   4,   5,   7, END],   # Edo 0 - edo inicial
+MT = [[1, IZQ, DER, ESP,   7,   6,   4,   4,   5,   7, END],   # Edo 0 - edo inicial
       [1, INT, INT, INT,   2,   7,   7,   7,   7,   7, INT],   # Edo 1 - digitos entertos
       [3, ERR, ERR, ERR,   7,   7,   7,   7,   7,   7, ERR],   # Edo 2 - primer decimal flotante
       [3, FLT, FLT, FLT,   7,   7,   7,   7,   7,   7, FLT],   # Edo 3 - decimales restantes
       [7, IDE, IDE, IDE,   7,   7,   4,   4,   7,   7, IDE],   # Edo 4 - simbolo
       [5,   5,   5,   5,   5,   7,   5,   5, STR,   7,   7],   # Edo 5 - string
       [7,   7,   7, ERR,   7,   7, BOL,   7,   7,   7,   7],   # Edo 6 - booleanos
-      [7,   7,   7, ERR,   7,   7,   7,   7,   7,   7,   7]]   # Edo 7 - Error
+      [7, ERR, ERR, ERR,   7,   7,   7,   7,   7,   7,   7]]   # Edo 7 - Error
 
 def filtro(c):
     # Regresa el numero de columna asociado al tipo de caracter dado(c)
@@ -36,6 +40,7 @@ def filtro(c):
     elif c == ')': # parentesis DER
         return 2
 
+        # espacio    tab            newline         otro 
     elif c == ' ' or ord(c) == 9 or ord(c) == 10 or ord(c) == 13: # blancos
         return 3
 
@@ -66,12 +71,13 @@ def filtro(c):
 
 _c = None # Siguiente caracter
 _leer = True # Indica si se requiere leer un caracter de la entrada estándar
+errorLex = False # Indica si hubo errores lexicos, si hay, cerramos el Script
 
 def obten_token():
     # Implementa un analizador lexico
-    global _c, _leer
+    global _c, _leer, errorLex
     edo = 0 # numero de estado en el automata
-    global lexema
+    global lexema # Hacemos el lexema global para que el parser pueda recibirlos
     lexema = "" # palabra que genera el token
     while (True):
         while edo < 100: # mientras que el estado no sea ACEPTOR o ERROR
@@ -82,48 +88,55 @@ def obten_token():
 
         if edo == INT:
             _leer = False # ya se leyo el siguiente caracter (delim)
-            # print("Num Entero:", lexema)
-            return INT#, lexema
+            #print("Num Entero:", lexema, INT)
+            return INT
 
         elif edo == FLT:
             _leer = False # ya se leyo el siguiente caracter (delim)
-            # print("Num Flotante:", lexema)
-            return FLT#, lexema
+            #print("Num Flotante:", lexema, FLT)
+            return FLT
 
         elif edo == IDE:
             _leer = False # ya se leyo el siguiente caracter (delim)
-            # print("Simbolo:", lexema)
-            return IDE#, lexema
+            #print("Simbolo:", lexema, IDE)
+            return IDE
 
         elif edo == STR:
             lexema += _c  # el ultimo caracter forma el lexema
-            # print("String:", lexema)
-            return STR#, lexema
+            #print("String:", lexema, STR)
+            return STR
 
         elif edo == IZQ:
             lexema += _c  # el ultimo caracter forma el lexema
-            # print("Parentesis IZQ:", lexema)
-            return IZQ#, lexema
+            #print("Parentesis IZQ:", lexema, IZQ)
+            return IZQ
 
         elif edo == DER:
             lexema += _c  # el ultimo caracter forma el lexema
-            # print("Parentesis DER:", lexema)
-            return DER#, lexema
+            #print("Parentesis DER:", lexema, DER)
+            return DER
 
         elif edo == BOL:
             lexema += _c  # el ultimo caracter forma el lexema
-            # print("Booleano:", lexema)
-            return BOL#, lexema
+            #print("Booleano:", lexema, BOL)
+            return BOL
 
         elif edo == END:
-            # print("Fin de expresion")
-            # lexema += _c
+            #print("Fin de expresion")
             return END
+
+        elif edo == ESP:
+            lexema += _c  # el ultimo caracter forma el lexema
+            edo = 0 # Dado a que en vez de que el esp actue solo regrese a 0, manualmente lo movemos a cero
+            parser.agregarEspacios(lexema) # Enviamos al codigo del parser los espacios para que ahi se puedan escribir al archivo
+            lexema = "" # Vaciamos el lexema para que el proximo lexema no reciba espacios
 
         else:
             _leer = False # el ultimo caracter no es raro
-            print("ERROR! palabra ilegal", lexema)
-            return ERR
-
+            print("ERROR LEXICO! palabra ilegal:", lexema)
+            parser.createHTML(ERR, lexema)
+            edo = 0
+            lexema = ""
+            errorLex = True
 
 
--- parserAI.py
+++ parserAI.py
@@ -1,11 +1,9 @@
-# <prog> ::= <exp> <prog> | $
-# <exp> ::= <atomo> | <lista>
-# <atomo> ::= simbolo | <constante>
-# <constante> ::= numero | booleano | string
-# <lista> ::= ( <elementos> )
-# <elementos> ::= <exp> <elementos> | e
+# Marcelo Márquez Murillo - A01720588
 
-inicio ='''<!DOCTYPE html>
+# Los prints fueron utilizados como debuggers
+
+# Inicio de cada codigo HTML
+inicio ='''<!DOCTYPE html> 
 <html>
     
 <head>
@@ -17,89 +15,194 @@ inicio ='''<!DOCTYPE html>
 <body>
 
 '''
+# Final de cada codigo HTML
 final = '''
 </body>
 
 </html>'''
 
 import sys
-import obten_tokenAI as scanner
+import obten_tokenAI as scanner # Importamos el codigo de obten_token
+
+# Abrimos el archivo HTML y le agregamos el inicio
+html = open("parser.html", "w")
+html.write(inicio)
+
+# Booleano para saber si el final ($) ya fue escrito o no para no ser repetido
+dlFinal = False
+
+# Bool para revisar si hay error sintactico
+errorSin = False
 
 # Empata y obtiene el siguiente token
 def match(tokenEsperado):
     global token
-    print("TOKEN:", scanner.lexema)
     if token == tokenEsperado:
-        #scanner.lexema = ""
+        #print("HTML: Escribimos token y lexema a HTML")
+        createHTML(token, scanner.lexema) # Cuando hacen match, enviamos el token y lexema para que puedan ser escritos en el HTML
         token = scanner.obten_token()
+        #print("LEXICO: Conseguimos siguiente token:", token, "lexema:", scanner.lexema)
     else:
-        error("Token Equivocado")
+        #print("Vamos a errorSintactico dentro MATCH")
+        errorSintactico()
 
 # Funcion principal: implementa el analisis sintactico
 def parser():
+    global errorSin
     global token
+    html.write('<p>\n') # Agregamos <p> para escribir un nuevo parrafo en HTML
     token = scanner.obten_token() # inicializa con el primer token
-    print("Vamos a PROG")
-    prog()
+    #print("Conseguimos primer token:", token, "lexema:", scanner.lexema)
+    #print("Vamos a PROG dentro PARSER")
+    prog() # Empezamos en Prog
 
 def prog():
+    global dlFinal
     if token == scanner.IDE or token == scanner.INT or token == scanner.FLT or token == scanner.BOL or token == scanner.STR or\
-        token == scanner.IZQ:
-        print("Vamos a EXP")
+        token == scanner.IZQ: # Revisamos que token esta seleccionado para saber a donde movernos
+        #print("Vamos a EXP dentro PROG")
         exp()
-        print("Vamos a PROG")
+        #print("Vamos a PROG dentro PROG")
         prog()
     elif token == scanner.END:
-        print("Expresion bien construida")
+        createHTML(token, '$') # Enviamos manualmente el $ dado que obten_token no lo envia
+        dlFinal = True
+        print("Expresion terminada")
+        if scanner.errorLex == True: # Revisamos si tuvimos algun error lexico por medio de errorLex
+            error("") # Dado que solo estamos tomado en consideracion el error lexico
+                            # llamamos error con nada que printear al igual que su segundo valor sera None
+        elif errorSin == True:
+            error("")
+        else:
+            html.write('\n</p>\n') # Agregamos </p> para cerrar el parrafo
+            quit = input("Exit? Y/N: ") # Preguntamos si quiere agregar mas codigo
+            if quit == 'Y' or quit == 'y':
+                html.write(final) # Escribimos el final del html
+                html.close() # Cerramos el archivo
+                sys.exit(1) # No salimos del Script
+            else:
+                #print("Volvemos a correr PARSER")
+                parser() # Volvemos a correr parser para seguir escribiendo
     else:
-        print("Expresion mal construida")
+        #print("Vamos a errorSintactico dentro PROG")
+        errorSintactico()
 
 def exp():
     if token == scanner.IDE or token == scanner.INT or token == scanner.FLT or token == scanner.BOL or token == scanner.STR:
-        print("Vamos a ATOMO")
+        # Revisamos que token esta seleccionado para saber a donde movernos
+        #print("Vamos a ATOMO dentro EXP")
         atomo()
     elif token == scanner.IZQ:
-        print("Vamos A LISTA")
+        # Revisamos que token esta seleccionado para saber a donde movernos
+        #print("Vamos a LISTA dentro EXP")
         lista()
     else:
-        error("Expresion mal iniciada")
+        #print("Vamos a errorSintactico dentro EXP")
+        errorSintactico()
 
 def atomo():
     if token == scanner.IDE:
         match(token)
     elif token == scanner.INT or token == scanner.FLT or token == scanner.BOL or token == scanner.STR:
-        print("Vamos a CONSTANTE")
+        # Revisamos que token esta seleccionado para saber a donde movernos
+        #print("Vamos a CONSTANTE dentro ATOMO")
         constante()
     else:
-        error("Expresion mal iniciada")
+        #print("Vamos a errorSintactico dentro ATOMO")
+        errorSintactico()
 
 def constante():
     if token == scanner.INT or token == scanner.FLT or token == scanner.BOL or token == scanner.STR:
         match(token)
     else:
-        error("Expresion mal iniciada")
+        #print("Vamos a errorSintactico dentro CONSTANTE")
+        errorSintactico()
 
 def lista():
     if token == scanner.IZQ:
         match(token)
-        print("Vamos a ELEMENTOS")
+        #print("Vamos a ELEMENTOS dentro LISTA")
         elementos()
-        match(scanner.DER)
+        if token == scanner.DER:
+            match(token)
+        else:
+            #print("Vamos a errorSintactico dentro LISTA por (")
+            errorSintactico()
     else:
-        error("Expresion mal iniciada")
+        #print("Vamos a errorSintactico dentro LISTA")
+        errorSintactico()
 
 def elementos():
+    global errorSin
     if token == scanner.IDE or token == scanner.INT or token == scanner.FLT or token == scanner.BOL or token == scanner.STR or\
-        token == scanner.IZQ:
-        print("Vamos a EXP")
+        token == scanner.IZQ: # Revisamos que token esta seleccionado para saber a donde movernos
+        #print("Vamos a EXP dentro ELEMENTOS")
         exp()
-        print("Vamos a ELEMENTOS")
+        #print("Vamos a ELEMENTOS dentro ELEMENTOS")
         elementos()
+    elif token == scanner.END:
+        errorSin = True
+        error("No se cerro lista")
+    #print("Vamos por VACIO")
+
+def errorSintactico():
+    global errorSin, token, dlFinal
+    dlFinal = False
+    errorSin = True
+    createHTML(scanner.ERR, scanner.lexema)
+    #print("Escribimos ERROR SINTACTICO:", scanner.lexema)
+    #scanner.edo = 0
+    token = scanner.obten_token() # Conseguir el siguiente token para poder seguir avanzando
+    #print("Conseguimos proximo token para seguir avanzando:", token, "lexema:", scanner.lexema)
+    #print("Regresamos a PROG dentro ERROR SINTACTICO para seguir avanzando")
+    prog()
+    error("Token equivocado")
 
 def error(mensaje):
-    print("ERROR:", mensaje)
+    global dlFinal
+    if errorSin == True:
+        if dlFinal == False:
+            createHTML(scanner.END, '$')
+        print("ERROR SINTACTICO:", mensaje)
+        html.write('\n</p>\n')
+        html.write('<p>\n<span class="err">&gt&gt ERROR SINTÁCTICO &lt&lt</span>\n</p>\n')
+        # Agregamos leyenda >> ERROR SINTACTICO <<
+    if scanner.errorLex == True:
+        html.write('<p>\n<span class="err">&gt&gt ERROR LEXICO &lt&lt</span>\n</p>\n')
+        # Agregamos leyenda >> ERROR LEXICO <<
+    html.write(final)
+    html.close()
     sys.exit(1)
 
-# def createHTML(token, lexema):
-#     html = open("parser.html", "w")
-#     html.write(inicio)
+def createHTML(token, lexema):
+    if token == scanner.IDE: # Si es IDE, escribimos su <span> con su lexema
+        html.write('<span class="ide">' + lexema + '</span>')
+    elif token == scanner.INT: # Si es INT, escribimos su <span> con su lexema
+        html.write('<span class="int">' + lexema + '</span>')
+    elif token == scanner.FLT: # Si es FLT, escribimos su <span> con su lexema
+        html.write('<span class="flt">' + lexema + '</span>')
+    elif token == scanner.IZQ: # Si es IZQ, escribimos su <span> con su lexema
+        html.write('<span class="par">' + lexema + '</span>')
+    elif token == scanner.DER: # Si es DER, escribimos su <span> con su lexema
+        html.write('<span class="par">' + lexema + '</span>')
+    elif token == scanner.BOL: # Si es BOL, escribimos su <span> con su lexema
+        html.write('<span class="bol">' + lexema + '</span>')
+    elif token == scanner.STR: # Si es STR, escribimos su <span> con su lexema
+        html.write('<span class="str">' + lexema + '</span>')
+    elif token == scanner.END: # Si es END, escribimos su <span> con su lexema
+        html.write('<span class="end">' + lexema + '</span>')
+    elif token == scanner.ERR: # Si es ERR, escribimos su <span> con su lexema
+        html.write('<span class="err">' + lexema + '</span>')
+
+def agregarEspacios(espacio):
+    # Para que el parser siga ignorando los espacios pero que aun los escriba en el html
+    # Cuando recibimos un espacio en obten_token llamamos esta función para agregarlos
+    if ord(espacio) == 9:
+        html.write('&nbsp;&nbsp;&nbsp;&nbsp;')
+    if ord(espacio) == 10:
+        html.write('<br>\n')
+    if espacio == ' ':
+        html.write('&nbsp;')
+
+
+
